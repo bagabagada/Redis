b40412d7db1c25baa40246d5b48c56bb89819526
bagabagada <1320394007@qq.com>
2026-04-22 21:00:26 +0800

    加入消息队列实现异步下单

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -1,12 +1,14 @@
 package com.hmdp.utils;
 
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
+@Component
 public class RedisIdWorker {
     /**
      * 开始时间戳
