180810de756a5aa93c2804fb8f3d73b045f1d8fb
bagabagada <1320394007@qq.com>
2026-04-22 21:02:30 +0800

    1 (#1)
    * 秒杀模块
    
    * 加入消息队列实现异步下单

---
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/config/RedisConfig.java
@@ -0,0 +1,19 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedissonClient redissonclient() {
+        //配置类
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://localhost:6379");
+        // 创建客户端
+        return Redisson.create(config);
+    }
+}
