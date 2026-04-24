1760cf8e6345a70339ecbbcd5c6c00d3afd70da1
bagabagada <1320394007@qq.com>
2026-04-24 11:35:11 +0800

    1 (#2)
    * 达人探店模块
    
    * 好友关注模块

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,11 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,4 +19,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/follow")
 public class FollowController {
 
+    @Resource
+    private IFollowService followService;
+
+    /**
+     * 查看是否关注
+     * @param followUserId
+     * @return
+     */
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
+
+    /**
+     * 关注followUserId的用户
+     * @param followUserId
+     * @param isFollow
+     * @return
+     */
+    @PutMapping("/{followUserId}/{id}")
+    public Result follow(@PathVariable("id") Long followUserId, @PathVariable Boolean isFollow) {
+        return followService.follow(followUserId,isFollow);
+    }
+
+    /**
+     * 查看共同关注
+     * @param id
+     * @return
+     */
+    @GetMapping("/common/{id}")
+    public Result followCommons(@PathVariable("id") Long id) {
+        return followService.followCommons(id);
+    }
 }
