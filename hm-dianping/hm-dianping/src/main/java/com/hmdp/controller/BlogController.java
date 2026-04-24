35e6ae58e10a9b8a71045c32cee96158064376ee
bagabagada <1320394007@qq.com>
2026-04-24 11:30:08 +0800

    好友关注模块

--- hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
+++ hm-dianping/hm-dianping/src/main/java/com/hmdp/controller/BlogController.java
@@ -32,17 +32,11 @@ public class BlogController {
 
     @PostMapping
     public Result saveBlog(@RequestBody Blog blog) {
-        // 获取登录用户
-        UserDTO user = UserHolder.getUser();
-        blog.setUserId(user.getId());
-        // 保存探店博文
-        blogService.save(blog);
-        // 返回id
-        return Result.ok(blog.getId());
+        return blogService.saveBlog(blog);
     }
 
     @GetMapping("/{id}")
-    public Result queryBlogById(@PathVariable Long id) {
+    public Result queryBlogById(@PathVariable("id") Long id) {
         return blogService.queryBlogById(id);
     }
 
@@ -52,7 +46,7 @@ public class BlogController {
     }
 
     @GetMapping("/likes/{id}")
-    public Result queryBlogLikes(@PathVariable Long id) {
+    public Result queryBlogLikes(@PathVariable("id") Long id) {
         return blogService.queryBlogLikes(id);
     }
 
@@ -71,5 +65,23 @@ public class BlogController {
     @GetMapping("/hot")
     public Result queryHotBlog(@RequestParam(value = "current", defaultValue = "1") Integer current) {
         return blogService.queryHotBlog(current);
+    }
+
+    //根据id查询博主的探店笔记
+    @GetMapping("/of/user")
+    public Result queryBlogByUserId(
+            @RequestParam(value = "current", defaultValue = "1") Integer current,
+            @RequestParam("id") Long id) {
+        // 根据用户查询
+        Page<Blog> page = blogService.query()
+                .eq("user_id", id).page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        return Result.ok(records);
+    }
+
+    @GetMapping("/of/follow")
+    public Result queryBlogOfFollow(@RequestParam(value = "offset",defaultValue = "0") Integer offset,@RequestParam("lastId") Long max) {
+        return blogService.queryBlogOfFollow(max,offset);
     }
 }
